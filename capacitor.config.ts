ab910a0385057ceb506ba484c28fa9df93d71985
TuroTV <romain.pins@gmail.com>
2024-02-07 15:07:37 +0100

    v0.0

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.edt4rt',
+  appName: 'edt4rt',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
