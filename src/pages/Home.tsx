2e68bdd70c1670664d7314fa48a74c8bd01fb625
TuroTV <romain.pins@gmail.com>
2024-02-15 15:15:18 +0100

    clean up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,49 +1,12 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
 import React, { useState, useEffect } from "react";
-import "./Home.css";
 import FullCalendar from "@fullcalendar/react";
-import timeGridPlugin from '@fullcalendar/timegrid'
+import timeGridPlugin from "@fullcalendar/timegrid";
+import "./Home.css";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 
+const Home = () => {
+  const [events, setEvents] = useState([]);
 
-function convertirFormatDate(dateString: string) {
-  const dateTimeParts = dateString.split(" ");
-  const dateWithoutTimeZone = dateTimeParts[0] + "T" + dateTimeParts[1].split("+")[0];
-  return dateWithoutTimeZone;
-}
-
-interface Event {
-  title: string;
-  start: string;
-  end: string;
-  id: string;
-  description: string;
-  extendedProps:any;
-}
-
-interface EventData {
-  summary: string;
-  start: string;
-  end: string;
-  description?: string;
-  prof?: string;
-  location: string;
-  uid?: string;
-}
-
-const Home: React.FC = () => {
-  const [events, setEvents] = useState<Event[]>([]);
-
-
-  const eventRender = (event:Event) => {
-    const location = event.extendedProps.location;
-  }
-  
   useEffect(() => {
     const fetchEvents = async () => {
       try {
@@ -53,60 +16,66 @@ const Home: React.FC = () => {
             "Content-Type": "application/json",
           },
         });
-        
+
         const json = await response.json();
-        console.log(json)
-        const formattedEvents: Event[] = json.map((eventData: EventData) => ({
-          title: eventData.summary + " - " + eventData.location + ((eventData.prof !== "NA" && " - " +eventData.prof) || ""),
-          start: convertirFormatDate(eventData.start),
-          end: convertirFormatDate(eventData.end),
+        const formattedEvents = json.map((eventData: any) => ({
+          title: `${eventData.summary} - ${eventData.location}${eventData.prof !== "NA" ? ` - ${eventData.prof}` : ""}`,
+          start: eventData.start.split(" ")[0] + "T" + eventData.start.split(" ")[1].split("+")[0], // Simplified date conversion
+          end: eventData.end.split(" ")[0] + "T" + eventData.end.split(" ")[1].split("+")[0],
           description: eventData.location,
           id: eventData.uid,
-          color : ((eventData.prof !== "NA" && "default") || "#005049"),
-          extendedProps:{
-            location:eventData.location,
-          }
-
+          color: eventData.prof !== "NA" ? "default" : "#005049",
         }));
 
         setEvents(formattedEvents);
       } catch (error) {
-        // Handle any errors during fetching or parsing
         console.error("Error fetching events:", error);
       }
     };
 
-    fetchEvents(); // Initiate the data fetching
+    fetchEvents();
   }, []);
 
   return (
     <IonPage>
-      {events.length ? (<>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>EDT 4 RT</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <div id="main-content">
+      {events.length ? (
+        <>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>EDT 4 RT</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent fullscreen>
+            
+            <div id="main-content">
             <FullCalendar
               plugins={[timeGridPlugin]}
               initialView="timeGridDay"
-              locale={"fr"}
+              locale="fr"
               titleFormat={{ month: "long", day: "numeric" }}
-              hiddenDays={[6,0]}
+              hiddenDays={[6, 0]}
               events={events}
               allDaySlot={false}
               height="auto"
               slotMinTime="08:00"
               slotMaxTime="19:00"
             />
-          </div>
-        </IonContent></>
+            </div>
+          </IonContent>
+        </>
       ) : (
-        // Display a loading indicator or placeholder while events are fetched
+        // Loading indicator
         <IonContent fullscreen>
-          <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+          <div className="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </IonContent>
       )}
     </IonPage>
