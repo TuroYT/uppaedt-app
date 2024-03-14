2459d0226c202ab11fc47fa50ff7ee1e463a76cb
Romain Pinsolle <romain.pins@gmail.com>
2024-03-14 09:11:47 +0100

    implémentation des autres groupes + amélioration de home.tsx + affichage des erreurs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,81 +1,25 @@
-import React, { useState, useEffect } from "react";
-import FullCalendar from "@fullcalendar/react";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import "./Home.css";
-import { CapacitorHttp } from '@capacitor/core';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import CalendarComponents from "../components/calendarComponents";
 
 const Home = () => {
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const response = await CapacitorHttp.get({url : "https://edt4rt-api.romain-pinsolle.fr/api/planning/getPlanningPerName/but1_g3"});
-        console.log(response.data)
-        const json = await response.data;
-        const formattedEvents = json.map((eventData: any) => ({
-          title: `${eventData.summary} - ${eventData.location}${eventData.prof !== "NA" ? ` - ${eventData.prof}` : ""}`,
-          start: eventData.start.split(" ")[0] + "T" + eventData.start.split(" ")[1].split("+")[0], // Simplified date conversion
-          end: eventData.end.split(" ")[0] + "T" + eventData.end.split(" ")[1].split("+")[0],
-          description: eventData.location,
-          id: eventData.uid,
-          color: eventData.prof !== "NA" ? "default" : "#005049",
-        }));
-
-
-
-        setEvents(formattedEvents);
-      } catch (error) {
-        console.error("Error fetching events:", error);
-      }
-    };
-
-    fetchEvents();
-  }, []);
-
   return (
     <IonPage>
-      {events.length ? (
-        <>
-          <IonHeader>
-            <IonToolbar>
-              <IonTitle>EDT 4 RT</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent fullscreen>
-            
-            <div id="main-content">
-            <FullCalendar
-              plugins={[timeGridPlugin]}
-              initialView="timeGridDay"
-              locale="fr"
-              titleFormat={{ month: "long", day: "numeric" }}
-              hiddenDays={[6, 0]}
-              events={events}
-              allDaySlot={false}
-              height="auto"
-              slotMinTime="08:00"
-              slotMaxTime="19:00"
-            />
-            </div>
-          </IonContent>
-        </>
-      ) : (
-        // Loading indicator
-        <IonContent fullscreen>
-          <div className="lds-roller">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        </IonContent>
-      )}
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>EDT 4 RT</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div id="main-content">
+          <CalendarComponents name="but1_g3"></CalendarComponents>
+        </div>
+      </IonContent>
     </IonPage>
   );
 };
